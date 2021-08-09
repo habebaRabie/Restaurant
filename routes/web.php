f651719a6fba51bcede8065384c9d5a9d8559fb8
Mariam Elkhashab <54447073+mariamkhashab@users.noreply.github.com>
2021-08-09 23:29:23 +0200

    authentication initial
    login and register with needed dependencies

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(); //helper function that generates all auth routes 
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
