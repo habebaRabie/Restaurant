9e02a78269f66daff5cd02eef89716f1c6757675
AsserOssama <asserossama33@gmail.com>
2021-09-02 10:52:16 +0200

    fixing Order model

--- app/Http/Controllers/showcontroller.php
+++ app/Http/Controllers/showcontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\orders;
+use App\Models\Order;
 use App\Models\Table;
 
 
@@ -46,7 +46,7 @@ class showcontroller extends Controller
      */
     function show()
     {
-       return $data=orders::all();
+       return $data=Order::all();
          
     }
 
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,5 +11,7 @@ class Order extends Model
 
     public function user(){
         return $this->belongsTo('App\User');
+        
     }
+    public $table = "orders";
 }
